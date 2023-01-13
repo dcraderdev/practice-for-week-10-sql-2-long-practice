5bd893fb6778ffa79dfaad4f2507de6a98114377
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-01-13 13:46:04 -0800

    phase 6 complete
    COMPLETED

--- sql-practice/phase-6.sql
+++ sql-practice/phase-6.sql
@@ -1,2 +1,16 @@
 -- Update the toy with the name of "Cheetos" to have a name of "Pooky"
--- Your code here
+
+
+UPDATE toys
+SET name = 'Pooky'
+WHERE name = 'Cheetos';
+
+
+-- UPDATE toys
+-- SET name = 'Cheetos'
+-- WHERE name = 'Pooky';
+
+
+
+SELECT toys.name
+FROM toys;
